d96813a974f147bbd873b56f9ba40832734dd894
Tiago Pereira de Faria <tiago_pereira_faria@hotmail.com>
2018-09-03 23:37:54 -0300

    Character movement

---
+++ Assets/Scripts/CharacterBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehavior : MonoBehaviour {
+    private Rigidbody2D rb;
+    public float speed;
+    private Vector2 Direction;
+    // Use this for initialization
+    void Start () {
+        this.rb = GetComponent<Rigidbody2D>();
+        this.Direction = new Vector2(0, 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.Move();
+        this.Atack();
+
+	}
+    
+    void Move()
+    {
+        int DirectionY = (int)Input.GetAxisRaw("Vertical");
+        int DirectionX = (int)Input.GetAxisRaw("Horizontal");
+
+        this.Direction = new Vector2(DirectionX, DirectionY);
+        this.rb.velocity = (this.Direction * this.speed);
+    }
+    void Atack()
+    {
+
+    }
+}
