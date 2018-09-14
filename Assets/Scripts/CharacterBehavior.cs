503c053390303878b31f6259df8f33e571e7a8f9
HeroSoukI <42747794+HeroSoukI@users.noreply.github.com>
2018-09-14 20:28:44 -0300

    teste

--- Assets/Scripts/CharacterBehavior.cs
+++ Assets/Scripts/CharacterBehavior.cs
@@ -6,35 +6,40 @@ public class CharacterBehavior : MonoBehaviour {
     public float speed;
     public float AttackDuration;
     public float AttackCouldownTime;
-    public int Damage;
+    public Object ObjAttack;
     private Rigidbody2D rb;
     private SpriteRenderer sprRenderer;
     private Vector2 Direction;
     private Animator animator;
     private bool AttackCouldown = false;
     private bool IsAttaking;
+    //som de ataque
+    public AudioClip impact;
+    AudioSource audioSource;
     // Use this for initialization
     void Start () {
         this.rb = GetComponent<Rigidbody2D>();
         this.sprRenderer = GetComponent<SpriteRenderer>();
         this.Direction = new Vector2(0, 1);
         this.animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
-
+	
 	// Update is called once per frame
 	void Update () {
         if(this.IsAttaking == false){
-            this.Move();
+            this.Move();  
         } else{
             this.rb.velocity = 0*this.rb.velocity;
         }
         if(Input.GetMouseButtonDown(0) && this.AttackCouldown == false){
             this.AttackCouldown = true;
             StartCoroutine(this.Attack());
-        }
+            audioSource.Play(0);
+    }
 
 	}
-
+    
     void Move()
     {
         int DirectionY = (int)Input.GetAxisRaw("Vertical");
@@ -49,18 +54,17 @@ public class CharacterBehavior : MonoBehaviour {
         Vector2 mouseDirection = (Vector2)worldMousePosition - (Vector2)transform.position;
         mouseDirection = mouseDirection.normalized;
 
-
+        
         var angle = Vector2.Angle(mouseDirection,Vector2.right);
         if(mouseDirection.y < 0 ) angle = 360 - angle;
         if(angle%45 > 22.5) angle = angle + 45 - angle%45;
         else angle = angle - angle%45;
-
+        
         animator.SetFloat("MouseDirectionX",Mathf.Cos(angle*Mathf.PI/180)*2);
         animator.SetFloat("MouseDirectionY",Mathf.Sin(angle*Mathf.PI/180)*2.5f);
         animator.SetBool("attaking",true);
         var AttackPosition = transform.position + new Vector3(Mathf.Cos(angle*Mathf.PI/180)*2,Mathf.Sin(angle*Mathf.PI/180)*2.5f,0);
-        GameObject atk = (GameObject)Instantiate(Resources.Load("attack"), AttackPosition, Quaternion.identity);
-        atk.GetComponent<PlayerAttackBehavior>().Damage = Damage;
+        var atk = Instantiate(ObjAttack, AttackPosition, Quaternion.identity);
         Destroy(atk,AttackDuration);
 
         this.IsAttaking = true;
--- Assets/Scripts/ProjectileBehavior.cs
+++ Assets/Scripts/ProjectileBehavior.cs
@@ -9,10 +9,13 @@ public class ProjectileBehavior : MonoBehaviour {
 	public float velocity;
 	public List<string> targets;
 	public float lifetime;
+    //som de hit
+    AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		this.transform.position += new Vector3(0,0,-5);
+        audioSource = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
@@ -26,6 +29,7 @@ public class ProjectileBehavior : MonoBehaviour {
 			other.GetComponent<HealthBehavior> ().ReceiveDMG (damage);
 			Destroy (gameObject);
 		}
+        audioSource.Play(0);
 	}
 
 	public IEnumerator Lifetime(){
