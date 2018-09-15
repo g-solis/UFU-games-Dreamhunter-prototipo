61ee27316f84667519837bd213ff36e568b1317a
Tiago Pereira de Faria <tiago_pereira_faria@hotmail.com>
2018-09-15 13:10:41 -0300

    Rolagem do personagem

--- Assets/Scripts/CharacterBehavior.cs
+++ Assets/Scripts/CharacterBehavior.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class CharacterBehavior : MonoBehaviour {
     public float speed;
     public float AttackDuration;
-    public float AttackCouldownTime;
+    public float AttackCooldownTime;
     public Object ObjAttack;
     private Rigidbody2D rb;
     private SpriteRenderer sprRenderer;
     private Vector2 Direction;
     private Animator animator;
-    private bool AttackCouldown = false;
-    private bool IsAttaking;
+    private bool AttackCooldown = false;
+    private bool CantMove;
+    //Variaveis para rolagem
+    private bool Rolling = false;
+    private float RollSpeed;
+    private Vector2 RollDirection;
+    public float RollMinSpeed; 
+    public float RollMaxSpeed;
+    public float RollTime;
+    public float RollCooldownTime;
+    private bool RollCooldown = false;
     //som de ataque
     public AudioClip impact;
     AudioSource audioSource;
@@ -27,19 +36,44 @@ public class CharacterBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(this.IsAttaking == false){
-            this.Move();  
+        if(this.CantMove == false){
+            this.Move();
         } else{
             this.rb.velocity = 0*this.rb.velocity;
         }
-        if(Input.GetMouseButtonDown(0) && this.AttackCouldown == false){
-            this.AttackCouldown = true;
+        if(Input.GetMouseButtonDown(0) && this.AttackCooldown == false){
+            this.AttackCooldown = true;
             StartCoroutine(this.Attack());
             audioSource.Play(0);
-    }
+        } 
+        else if(Input.GetKeyDown("space") && !this.RollCooldown){
+            StartCoroutine(StartRolling());
+        }
+        if(this.Rolling){
+            this.Roll();
+        }
+        
 
 	}
-    
+    IEnumerator StartRolling(){
+        this.Rolling = true;
+        this.RollCooldown = true;
+        this.CantMove = true;
+        this.RollSpeed = this.RollMaxSpeed;
+        var worldMousePosition = Camera.main.ScreenToWorldPoint( Input.mousePosition);
+        this.RollDirection = (Vector2)worldMousePosition - (Vector2)transform.position;
+        this.RollDirection = this.RollDirection.normalized;
+        yield return new WaitForSeconds((float)RollTime);
+        this.Rolling = false;
+        this.CantMove = false;
+        yield return new WaitForSeconds((float)RollCooldownTime);
+        this.RollCooldown = false;
+    }
+    void Roll(){
+        this.rb.velocity = (this.RollDirection * this.RollSpeed);
+        this.RollSpeed -= (this.RollSpeed - this.RollMinSpeed) / 20;
+        Debug.Log(this.RollSpeed);
+    }
     void Move()
     {
         int DirectionY = (int)Input.GetAxisRaw("Vertical");
@@ -67,11 +101,11 @@ public class CharacterBehavior : MonoBehaviour {
         var atk = Instantiate(ObjAttack, AttackPosition, Quaternion.identity);
         Destroy(atk,AttackDuration);
 
-        this.IsAttaking = true;
+        this.CantMove = true;
         yield return new WaitForSeconds(AttackDuration);
         animator.SetBool("attaking",false);
-        this.IsAttaking = false;
-        yield return new WaitForSeconds((float)AttackCouldownTime);
-        this.AttackCouldown = false;
+        this.CantMove = false;
+        yield return new WaitForSeconds((float)AttackCooldownTime);
+        this.AttackCooldown = false;
     }
 }
